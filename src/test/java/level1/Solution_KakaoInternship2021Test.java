a48445eead4c1875aece335ea718c318b1d865f9
icolaaa <zr.colaaa@gmail.com>
2022-05-19 14:01:29 +0900

    :white_check_mark: [PGS] Pass tests(#81301)

---
+++ src/test/java/level1/Solution_KakaoInternship2021Test.java
@@ -0,0 +1,22 @@
+package level1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Solution_KakaoInternship2021Test {
+
+    private final Solution_KakaoInternship2021 sk = new Solution_KakaoInternship2021();
+
+    @Test
+    void solution01() {
+        //given
+        String s = "2three45sixseven";
+
+        //when
+        int result = sk.solution01(s);
+
+        //then
+        assertEquals(result, 234567);
+    }
+}
