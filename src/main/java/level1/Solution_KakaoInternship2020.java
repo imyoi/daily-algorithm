c82417f1a5764f8f6c9aa292f4cd6852b3b3f543
icolaaa <zr.colaaa@gmail.com>
2022-05-19 09:29:47 +0900

    :bulb: [PGS] Update comments(#67256)

--- src/main/java/level1/Solution_KakaoInternship2020.java
+++ src/main/java/level1/Solution_KakaoInternship2020.java
@@ -8,11 +8,10 @@ public class Solution_KakaoInternship2020 {
      * #67256
      * 순서대로 누를 번호가 담긴 배열 numbers, 왼손잡이인지 오른손잡이인 지를 나타내는 문자열 hand 가 매개변수로 주어질 때,
      * 각 번호를 누른 엄지손가락이 왼손인 지 오른손인 지를 나타내는 연속된 문자열 형태로 return 하도록 solution 함수를 완성해주세요.
-     * [제한사항] 왼손(L), 오른손(R)을 순서대로 이어붙여 문자열 형태로 return 해주세요.
-     *   - 키패드 이동 한 칸의 거리 = 1
-     *   - 엄지손가락은 상하좌우 4가지 방향으로만 이동할 수 있음 !
-     *   - 가운데 열은 두 엄지손가락의 현재 키패드의 위치에서 더 가까운 엄지손가락을 사용하고,
-     *   - 두 엄지손가락의 거리가 같다면 오른손잡이는 오른손 엄지손가락, 왼손잡이는 왼손 엄지손가락을 사용합니다.
+     * [제한사항]
+     *  - 엄지손가락은 상하좌우로만 이동 가능 (키패드 이동 한 칸의 거리 = 1)
+     *  - 가운데 열은 두 엄지손가락의 현재 키패드의 위치에서 더 가까운 엄지손가락을 사용하고,
+     *  - 두 엄지손가락의 거리가 같다면 오른손잡이는 오른손 엄지손가락, 왼손잡이는 왼손 엄지손가락을 사용합니다.
      */
     public String solution01(int[] numbers, String hand) {
         String answer = "";
@@ -25,15 +24,15 @@ public class Solution_KakaoInternship2020 {
         for (int n : numbers) {
             if (n == 0) n = 11;
             switch (n % 3) {
-                case 0: //3,6,9,12 → "R"
+                case 0: //3,6,9,12
                     answer += "R";
                     right.push(n);
                     break;
-                case 1: //1,4,7,10 → "L"
+                case 1: //1,4,7,10
                     answer += "L";
                     left.push(n);
                     break;
-                case 2: //거리비교
+                case 2:
                     int ld = getDistance(n, left.peek());
                     int rd = getDistance(n, right.peek());
 
@@ -47,9 +46,7 @@ public class Solution_KakaoInternship2020 {
                         answer += hand.substring(0,1).toUpperCase();
                         if("left".equals(hand)) {
                             left.push(n);
-                        }else {
-                            right.push(n);
-                        }
+                        } else right.push(n);
                     }
                     break;
             }
@@ -57,8 +54,9 @@ public class Solution_KakaoInternship2020 {
         return answer;
     }
 
+    // 거리계산 : 좌우(1) + 상하(3)
     public static int getDistance(int n, int last) {
-        int distance = Math.abs(n - last)%3 + Math.abs(n - last)/3; //상하(3) + 좌우(1)
-        return distance;
+        return Math.abs(n - last)%3 + Math.abs(n - last)/3;
     }
+
 }
