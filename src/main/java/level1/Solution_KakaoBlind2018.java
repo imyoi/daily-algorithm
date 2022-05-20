7b3e936e29e2bb2b07ba02420e67e5ebe6fa1e7a
icolaaa <104837715+icolaaa@users.noreply.github.com>
2022-05-21 01:30:09 +0900

    :sparkles: [PGS] Add [1차] 다트게임(#17682)

--- src/main/java/level1/Solution_KakaoBlind2018.java
+++ src/main/java/level1/Solution_KakaoBlind2018.java
@@ -1,6 +1,12 @@
 package level1;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Solution_KakaoBlind2018 {
+
+    private static Pattern pattern = Pattern.compile("([0-9]+)([a-zA-Z])([*#]?)");
 
     /**
      * #17681
@@ -22,4 +28,53 @@ public class Solution_KakaoBlind2018 {
         return answer;
     }
 
+    /**
+     * #17682
+     * 0~10의 정수와 문자 S, D, T, *, #로 구성된 문자열이 입력될 시 총점수를 반환하는 함수를 작성하라.
+     * => "점수|보너스|[옵션]" 문자열 3세트
+     * - 점수 : 0점에서 10점
+     * - 보너스 : Single(S), Double(D), Triple(T)
+     *          => 점수에서 1제곱, 2제곱, 3제곱으로 계산
+     *          => 점수마다 하나씩 존재한다.
+     * - 옵션은 스타상(*) → 해당 점수와 바로 전에 얻은 점수를 각 2배로 만든다 , 아차상(#) → x마이너스 로 점수마다 둘 중 하나만 존재할 수 있으며, 없을 수도 있다.
+     * */
+    public int solution02(String dartResult) {
+        int[] score = getScore(dartResult);
+        return Arrays.stream(score).sum();
+    }
+
+    //입력값, 위치, 찾을패턴
+    public int[] getScore(String input) {
+        int[] score = new int[3];
+        int m = 0;
+
+        Matcher mathcer = pattern.matcher(input);
+        while(mathcer.find()){
+            score[m] = Integer.parseInt(mathcer.group(1)); //게임1
+
+            switch (mathcer.group(2)) { //게임2
+                case "D":
+                    score[m] = (int) Math.pow(score[m],2);
+                    break;
+                case "T":
+                    score[m] = (int) Math.pow(score[m],3);
+                    break;
+            }
+
+            switch (mathcer.group(3)) { //게임3
+                case "*":
+                    if(m != 0){
+                        score[m-1] *= 2; //전에 얻은 점수
+                    }
+                    score[m] *= 2;
+                    break;
+                case "#":
+                    score[m] *= -1;
+                    break;
+            }
+            m++;
+        }
+
+        return score;
+    }
 }
