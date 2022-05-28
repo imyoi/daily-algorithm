f95fc3636fb19dcdd5ccc63cde2aff6352efd1e3
icolaaa <104837715+icolaaa@users.noreply.github.com>
2022-05-28 23:38:46 +0900

    :white_check_mark: [PGS] Pass test(#42748)

--- src/test/java/level1/Solution_AlgorithmKitTest.java
+++ src/test/java/level1/Solution_AlgorithmKitTest.java
@@ -2,6 +2,7 @@ package level1;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Solution_AlgorithmKitTest {
@@ -19,5 +20,19 @@ class Solution_AlgorithmKitTest {
 
         //then
         assertEquals(result, "mislav");
+    }
+
+    @Test
+    void Solution02() {
+        //given
+        int[] array = {1,5,2,6,3,7,4};
+        int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
+
+        //when
+        int[] answer = sa.solution02(array, commands);
+        int[] result = {5,6,3};
+
+        //then
+        assertArrayEquals(result, new int[]{5,6,3});
     }
 }
