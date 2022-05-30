0fb6f1dc5385155da923377c1274cd74c57cae46
imyoi <zr.colaaa@gmail.com>
2022-05-30 11:40:47 +0900

    :white_check_mark: [PGS] Pass test(#42840)

--- src/test/java/level1/Solution_AlgorithmKitTest.java
+++ src/test/java/level1/Solution_AlgorithmKitTest.java
@@ -35,4 +35,32 @@ class Solution_AlgorithmKitTest {
         //then
         assertArrayEquals(result, new int[]{5,6,3});
     }
+
+    @Test
+    void solution03() {
+        //given
+        int[] answers = {1,2,3,4,5};
+        int[] answers2 = {1,3,2,4,2};
+        int[] answers3 = {5,5,1,4,1};
+        int[] answers4 = {1,2,1,1,2};
+
+        int[] answers5 = {5,4,4,2,1};
+        int[] answers6 = {3,3,1,1,1,1,2,3,4,5};
+
+        //when
+        int[] result = sa.solution03(answers);
+        int[] result2 = sa.solution03(answers2);
+        int[] result3 = sa.solution03(answers3);
+        int[] result4 = sa.solution03(answers4);
+        int[] result5 = sa.solution03(answers5);
+        int[] result6 = sa.solution03(answers6);
+
+        //then
+        assertArrayEquals(result, new int[]{1});
+        assertArrayEquals(result2, new int[]{1,2,3});
+        assertArrayEquals(result3, new int[]{1,3});
+        assertArrayEquals(result4, new int[]{3});
+        assertArrayEquals(result5, new int[]{});
+        assertArrayEquals(result6, new int[]{1,3});
+    }
 }
