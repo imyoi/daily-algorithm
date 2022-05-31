1f8591c27529920cdf5673fa07fe8e1c122ef5f9
imyoi <zr.colaaa@gmail.com>
2022-05-31 21:10:11 +0900

    :sparkles: [PGS] Add 2016(#12901)

---
+++ src/main/java/level1/Solution_Practice.java
@@ -0,0 +1,20 @@
+package level1;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+public class Solution_Practice {
+
+    /**
+     * #12901
+     * @param a : 월
+     * @param b : 일
+     * @return 2016년 a월 b일이 무슨 요일인지 리턴
+     * */
+    public String solution01(int a, int b) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(2016,a-1, b); //month 숫자로 입력시 0이 1월이기 때문에 -1을 해줌
+
+        return cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US).toUpperCase();
+    }
+}
