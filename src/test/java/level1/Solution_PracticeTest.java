1b86608c232b96a4ad2af57dec4c1cce0ff4ad37
imyoi <zr.colaaa@gmail.com>
2022-06-02 21:24:53 +0900

    :white_check_mark: [PGS] Pass test(#12903)

--- src/test/java/level1/Solution_PracticeTest.java
+++ src/test/java/level1/Solution_PracticeTest.java
@@ -9,7 +9,7 @@ class Solution_PracticeTest {
     private final Solution_Practice sp = new Solution_Practice();
 
     @Test
-    void solution01() {
+    void solution01_2016() {
         //given
         int a = 5;
         int b = 24;
@@ -19,5 +19,20 @@ class Solution_PracticeTest {
 
         //then
         assertEquals(result, "TUE");
+    }
+
+    @Test
+    void solution02_가운데_글자_가져오기() {
+        //given
+        String s = "abcde";
+        String s2 = "qwer";
+
+        //when
+        String result = sp.solution02(s);
+        String result2 = sp.solution02(s2);
+
+        //then
+        assertEquals(result, "c");
+        assertEquals(result2, "we");
     }
 }
