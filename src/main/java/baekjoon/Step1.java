7f038e5a5aaa676c81c493da496ecef3c42a9d16
icolaaa <104837715+icolaaa@users.noreply.github.com>
2022-06-05 23:20:31 +0900

    :sparkles: [BOJ] Add Hello Wolrd(#2557)

---
+++ src/main/java/baekjoon/Step1.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+/**
+ * 입출력과 사칙연산
+ * */
+public class Step1 {
+
+    /**
+     * #2557
+     * Hello World!를 출력하시오.
+     * */
+    public void solution01() {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/baekjoon/Step1Test.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import org.junit.jupiter.api.Test;
+class Step1Test {
+
+    private final Step1 s = new Step1();
+
+    @Test
+    void solution01_HelloWolrd_출력() {
+        s.solution01();
+    }
+}
