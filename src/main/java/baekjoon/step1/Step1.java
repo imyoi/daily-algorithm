332859fd23771dee7795863ed61c5f297c163799
imyoi <zr.colaaa@gmail.com>
2022-06-06 23:16:10 +0900

    :sparkles: [BOJ] Add We love kriii(#10718)

---
+++ src/main/java/baekjoon/step1/Step1.java
@@ -0,0 +1,24 @@
+package baekjoon.step1;
+
+/**
+ * 입출력과 사칙연산
+ * */
+public class Step1 {
+
+    /**
+     * #2557
+     * Hello World!를 출력하시오.
+     * */
+    public void solution01() {
+        System.out.println("Hello World!");
+    }
+
+    /**
+     * #10718
+     * 두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한 번씩 출력한다.
+     * */
+    public void solution02() {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
