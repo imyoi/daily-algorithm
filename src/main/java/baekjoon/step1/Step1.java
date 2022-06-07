603c25a4bdc06ce035ad6dce894188c27c35186c
imyoi <zr.colaaa@gmail.com>
2022-06-07 23:09:47 +0900

    ✨ [BOJ] Add 사칙연산(#1000,#1001,#10998,#1008)

--- src/main/java/baekjoon/step1/Step1.java
+++ src/main/java/baekjoon/step1/Step1.java
@@ -1,9 +1,15 @@
 package baekjoon.step1;
 
+import java.util.Scanner;
+
 /**
  * 입출력과 사칙연산
  * */
 public class Step1 {
+
+    public static void main(String[] args) {
+        solution06();
+    }
 
     /**
      * #2557
@@ -36,5 +42,34 @@ public class Step1 {
      * */
     public void solution04() {
         System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+    }
+
+    /**
+     * #1000, #1001, #10998
+     * 두 정수 A와 B를 입력받은 다음, A+B(또는 A-B, AxB)를 출력하는 프로그램을 작성하시오.
+     * */
+    public static void solution05() {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A+B); //#1000
+        System.out.println(A-B); //#1001
+        System.out.println(A*B); //#10998
+
+        sc.close(); //자원해제
+    }
+
+    /**
+     * #1008
+     * 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+     * */
+    public static void solution06() {
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+
+        System.out.println(A/B); //#1008
+        sc.close(); //자원해제
     }
 }
