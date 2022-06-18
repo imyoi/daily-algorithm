7546d1de6bf7727e646a5133890d75948738da38
imyoi <zr.colaaa@gmail.com>
2022-06-18 22:45:03 +0900

    ✨ [PGS] Add 연습문제(#12943,#12944,#12947)

--- src/main/java/programmers/level1/Solution_Practice.java
+++ src/main/java/programmers/level1/Solution_Practice.java
@@ -456,4 +456,58 @@ public class Solution_Practice {
         if(min == 0) return max;
         return gcd(min, max%min);
     }
+
+    /**
+     * #12943
+     * ? 주어진 수가 1이 될 때까지 다음 작업을 반복하면, 모든 수를 1로 만들 수 있다는 추측
+     * 1-1. 입력된 수가 짝수라면 2로 나눕니다.
+     * 1-2. 입력된 수가 홀수라면 3을 곱하고 1을 더합니다.
+     * 2. 결과로 나온 수에 같은 작업을 1이 될 때까지 반복합니다.
+     * ✓ 단, 주어진 수가 1인 경우에는 0을, 작업을 500번 반복할 때까지 1이 되지 않는다면 –1을 반환해 주세요.
+     * ✓ 입력된 수, num은 1 이상 8,000,000 미만인 정수입니다.
+     * @param num
+     * @return 몇 번이나 반복해야 하는지 반환
+     * */
+    public int solution24(long num) {
+        int answer = 0;
+        if(num == 1) return 0;
+
+        while(num != 1) {
+            if(answer == 500) return -1;
+            if(num % 2 == 0) {
+                num = num / 2;
+            }else {
+                num = num * 3 + 1;
+            }
+            answer++;
+        }
+        return answer;
+    }
+
+    /**
+     * #12944
+     * ✓ arr의 원소는 -10,000 이상 10,000 이하인 정수입니다.
+     * @param arr : arr은 길이 1 이상, 100 이하인 배열입니다.
+     * @return 정수를 담고 있는 배열 arr의 평균값을 return
+     * */
+    public double solution25(int[] arr) {
+        return Arrays.stream(arr).average().getAsDouble();
+    }
+
+    /**
+     * #12947
+     * ? 양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다.
+     * @param x : 1 이상, 10000 이하인 정수입니다.
+     * @return 자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수
+     * */
+    public boolean solution26(int x) {
+        int n = x;
+        int sum = 0;
+        while(n > 0){
+            sum += n % 10;
+            n /= 10;
+        }
+        if(x % sum != 0) return false;
+        return true;
+    }
 }
