615e7ff3253565f40365b0db4b3f299e8c6c2033
imyoi <zr.colaaa@gmail.com>
2022-06-21 23:53:52 +0900

    ✨ [BOJ] Add 입출력과 사칙연산(#10869,#18108,#10430,#2588)

--- src/main/java/baekjoon/step1/Step1.java
+++ src/main/java/baekjoon/step1/Step1.java
@@ -1,5 +1,8 @@
 package baekjoon.step1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /**
@@ -8,7 +11,7 @@ import java.util.Scanner;
 public class Step1 {
 
     public static void main(String[] args) {
-        solution06();
+        solution11();
     }
 
     /**
@@ -71,5 +74,73 @@ public class Step1 {
 
         System.out.println(A/B); //#1008
         sc.close(); //자원해제
+    }
+
+    /**
+     * #10869
+     * 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
+     * */
+    public static void solution07() {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
+
+    /**
+     * #10926
+     * 준하가 가입하려고 하는 사이트에 이미 존재하는 아이디가 주어졌을 때, 놀람을 표현하는 프로그램을 작성하시오.
+     * */
+    public static void solution08() {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        System.out.println(id + "??!");
+    }
+
+    /**
+     * #18108
+     * 불기 연도 y가 주어질 때 이를 서기 연도로 바꿔 주는 프로그램을 작성하시오.
+     * ! Scanner는 정규식 검사가 오래걸리기 때문에 BufferedReader를 사용
+     * */
+    public static void solution09() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int y = Integer.parseInt(br.readLine()) - 543;
+        System.out.println(y);
+    }
+
+    /**
+     * #10430
+     * 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+     * */
+    public static void solution10() {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C) * (B%C))%C);
+    }
+
+    /**
+     * #2588
+     * (세 자리 수) × (세 자리 수) 연산 중 첫째 줄부터 넷째 줄까지 차례대로 들어갈 값을 출력한다.
+     * */
+    public static void solution11() {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println(num1 * (num2%10));
+        System.out.println(num1 * (num2%100/10));
+        System.out.println(num1 * (num2/100));
+        System.out.println(num1 * num2);
     }
 }
