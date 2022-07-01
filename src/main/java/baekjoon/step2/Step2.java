3d0b97487f0d83f9cc7eccb4175b44d7d4aaf53f
imyoi <zr.colaaa@gmail.com>
2022-07-01 23:42:18 +0900

    ✨ [BOJ] Add 오븐 시계(#2525), 주사위 세개(#2480)

--- src/main/java/baekjoon/step2/Step2.java
+++ src/main/java/baekjoon/step2/Step2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Step2 {
 
     public static void main(String[] args) {
-        p_2884();
+        p_2480();
     }
 
     /**
@@ -79,5 +79,54 @@ public class Step2 {
             else h--;
         }
         System.out.println(h + " " + m);
+    }
+
+    /**
+     * 훈제오리구이를 시작하는 시각과 오븐구이를 하는 데 필요한 시간이 분단위로 주어졌을 때,
+     * 오븐구이가 끝나는 시각을 계산하는 프로그램을 작성하시오.
+     * */
+    public static void p_2525() {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        int t = sc.nextInt();
+
+        h += t / 60;
+        m += t % 60;
+
+        if (m >= 60) {
+            h += 1;
+            m -= 60;
+        }
+        if (h >= 24) h -= 24;
+        System.out.println(h + " "+ m);
+    }
+
+    /**
+     * 3개 주사위의 나온 눈이 주어질 때, 상금을 계산하는 프로그램을 작성 하시오.
+     * 1) 같은 눈이 3개가 나오면 10,000원+(같은 눈)×1,000원의 상금을 받게 된다.
+     * 2) 같은 눈이 2개만 나오는 경우에는 1,000원+(같은 눈)×100원의 상금을 받게 된다.
+     * 3) 모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈)×100원의 상금을 받게 된다.
+     * */
+    public static void p_2480() {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a==b && a==c) {
+            System.out.println(10000 + a*1000);
+        }else if(a==b && a!=c ) {
+            System.out.println(1000 + a*100);
+        }else if(a==c && a!=b ) {
+            System.out.println(1000 + a*100);
+        }else if(b==c && a!=b ) {
+            System.out.println(1000 + b*100);
+        }else {
+            int max = a;
+            if (max < b ) max = b;
+            if (max < c ) max = c;
+            System.out.println( max*100);
+        }
     }
 }
